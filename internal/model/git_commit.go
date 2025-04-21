21216643d28eed5c0372435f754e37f759d8f6d4
Beriholic <beriholic@petalmail.com>
2025-04-21 11:28:51 +0800

    :bug: fix: swap type and emoji in git commit string method

--- internal/model/git_commit.go
+++ internal/model/git_commit.go
@@ -12,12 +12,12 @@ type GitCommit struct {
 func (g GitCommit) String() string {
 	if g.Scope != "" {
 		if g.Emoji != "" {
-			return fmt.Sprintf("%s %s(%s): %s", g.Emoji, g.Typ, g.Scope, g.Msg)
+			return fmt.Sprintf("%s %s(%s): %s", g.Typ, g.Emoji, g.Scope, g.Msg)
 		}
 		return fmt.Sprintf("%s(%s): %s", g.Typ, g.Scope, g.Msg)
 	}
 	if g.Emoji != "" {
-		return fmt.Sprintf("%s %s: %s", g.Emoji, g.Typ, g.Msg)
+		return fmt.Sprintf("%s %s: %s", g.Typ, g.Emoji, g.Msg)
 	}
 	return fmt.Sprintf("%s: %s", g.Typ, g.Msg)
 }
